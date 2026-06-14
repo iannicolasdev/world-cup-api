8bc81147b4f80a969f1a59340a24d05b7fd62a03
iannicolasdev <nicolasian002@gmail.com>
2026-06-14 17:48:04 -0300

    feat: add endpoint to list all World Cups

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort
+from database import world_cups
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return jsonify({
         "status": "online",
-        "message": "The World Cup API is running.!"
+        "message": "The World Cup API is running!"
     })
 
+
+@app.route("/world-cups")
+def list_cups():
+    return jsonify(world_cups)
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
