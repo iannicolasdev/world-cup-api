04976e3244ae3a4ae66a0fa57c289f15cc432aea
iannicolasdev <nicolasian002@gmail.com>
2026-06-14 17:45:56 -0300

    feat: add World Cup dataset from 2002 to 2022

---
+++ database.py
@@ -0,0 +1,44 @@
+world_cups = [
+    {
+        "edition": 2002,
+        "host": "South Korea and Japan",
+        "winner": "Brazil",
+        "top_scorer": "Ronaldo",
+        "star_players": ["Ronaldo", "Ronaldinho", "Rivaldo", "Cafu"]
+    },
+    {
+        "edition": 2006,
+        "host": "Germany",
+        "winner": "Italy",
+        "top_scorer": "Miroslav Klose",
+        "star_players": ["Fabio Cannavaro", "Gianluigi Buffon", "Zinedine Zidane"]
+    },
+    {
+        "edition": 2010,
+        "host": "South Africa",
+        "winner": "Spain",
+        "top_scorer": "Thomas Müller",
+        "star_players": ["Andrés Iniesta", "Xavi", "Iker Casillas"]
+    },
+    {
+        "edition": 2014,
+        "host": "Brazil",
+        "winner": "Germany",
+        "top_scorer": "James Rodríguez",
+        "star_players": ["Thomas Müller", "Manuel Neuer", "Lionel Messi"]
+    },
+    {
+        "edition": 2018,
+        "host": "Russia",
+        "winner": "France",
+        "top_scorer": "Harry Kane",
+        "star_players": ["Kylian Mbappé", "Luka Modrić", "Antoine Griezmann"]
+    },
+    {
+        "edition": 2022,
+        "host": "Qatar",
+        "winner": "Argentina",
+        "top_scorer": "Kylian Mbappé",
+        "star_players": ["Lionel Messi", "Kylian Mbappé", "Julián Álvarez"]
+    }
+]
